1fbd541853cd948707de940b320867f0d9ad6116
matyMusic <mtyg7702@gmail.com>
2022-09-22 13:09:14 +0300

    portfolio

---
+++ src/assets/js/threeBg.js
@@ -0,0 +1,35 @@
+// import * as THREE from 'three';
+// const container = document.querySelector('.three_bg');
+// const loader = new THREE.TextureLoader
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(
+//   70,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+// const renderer = new THREE.WebGL1Renderer({
+//   antialias: true,
+// });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// container.appendChild(renderer.domElement);
+
+// const geometry = new THREE.PlaneGeometry(10, 5);
+
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+// camera.position.z = 5;
+
+// function animate() {
+//   requestAnimationFrame(animate);
+//   //   mesh.rotation.x += 0.01;
+//   //   mesh.rotation.y += 0.01;
+//   renderer.render(scene, camera);
+
+//   renderer.render(scene, camera);
+// }
+// animate();
